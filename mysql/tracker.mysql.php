b976efaf36df0417cfca5eeb20d8d4468c3f81b8
AkBKukU <shelbyjueden@gmail.com>
2014-03-29 08:46:57 -0700

    Started work on peer viewing page

--- mysql/tracker.mysql.php
+++ mysql/tracker.mysql.php
@@ -43,7 +43,7 @@ $_SERVER['tracker'] = array(
 	// database options
 	'db_host'           => 'localhost',   /* ip or hostname to mysql server */
 	'db_user'           => 'root',        /* username used to connect to mysql */
-	'db_pass'           => '',            /* password used to connect to mysql */
+	'db_pass'           => 'password',            /* password used to connect to mysql */
 	'db_name'           => 'peertracker', /* name of the PeerTracker database */
 
 	// advanced database options
